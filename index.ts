0e68b11686fb5ff7285b595f994d2f94de83c7bf
Richard Wang <rwang83@gmail.com>
2021-03-16 00:23:11 -0400

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 /// <reference path="./types.d.ts" />
 
 export * from './src/util/render';
+export * from './src/dual3/dual-march';
 export * from './src/csg/primitives';
 export * from './src/csg/boolean';
 export * from './src/csg/extrude';
