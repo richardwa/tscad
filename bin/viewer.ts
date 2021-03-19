d657b3c39f283150708530411b11ebffecf3b88f
Richard Wang <rwang83@gmail.com>
2021-03-18 20:01:59 -0400

    fix node viewer

--- bin/viewer.ts
+++ bin/viewer.ts
@@ -7,29 +7,38 @@ import * as fs from 'fs';
 import * as open from 'open';
 
 const cwd = process.cwd();
+const dir = path.join(__dirname, '../dist/viewer');
+const homePage = fs.readFileSync(path.join(dir, 'viewer.html'));
 
 const requestListener: http.RequestListener = (req, res) => {
   console.log(new Date().toISOString(), req.method, req.url);
-  const homePage = fs.readFileSync(path.join(__dirname, '../dist/viewer/viewer.html'));
   if (req.url === '/') {
     res.writeHead(200);
     res.end(homePage);
     return;
   }
-  const file = path.join(cwd, req.url);
-  console.log(file);
-  if (fs.existsSync(file)) {
-    import(file)
+  // local file wrt this file
+  const dirFile = path.join(dir, req.url);
+  if (fs.existsSync(dirFile)) {
+    res.writeHead(200);
+    res.end(fs.readFileSync(dirFile, 'utf8'));
+    return;
+  }
+
+  const cwdFile = path.join(cwd, req.url);
+  console.log(cwdFile);
+  if (fs.existsSync(cwdFile)) {
+    import(cwdFile)
       .then(({ main }) => {
         res.writeHead(200);
         res.end(homePage + main);
       }).catch((err) => {
         res.writeHead(500);
-        res.end([file, err].join('\n'));
+        res.end([cwdFile, err].join('\n'));
       });
   } else {
     res.writeHead(404);
-    res.end(file + " not found");
+    res.end(cwdFile + " not found");
   }
 };
 
