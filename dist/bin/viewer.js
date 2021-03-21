d7c861243333badaf9f082c7a07880af07705048
Richard Wang <rwang83@gmail.com>
2021-03-20 20:45:28 -0400

    update

--- dist/bin/viewer.js
+++ dist/bin/viewer.js
@@ -5,7 +5,6 @@ exports.__esModule = true;
 var http = require("http");
 var path = require("path");
 var fs = require("fs");
-var open = require("open");
 var glsl_util_1 = require("../src/csg/glsl-util");
 var cwd = process.argv.length > 2 ? process.argv[2] : process.cwd();
 var dir = path.join(__dirname, '../dist/viewer');
@@ -30,7 +29,6 @@ var requestListener = function (req, res) {
     if (fs.existsSync(cwdFile)) {
         Promise.resolve().then(function () { return require(cwdFile); }).then(function (_a) {
             var main = _a.main;
-            console.log(main);
             var shaderSrc = glsl_util_1.getShaderSrc(main.gl);
             var script = "<script>window.shaderSrc = " + JSON.stringify(shaderSrc) + ";</script>";
             res.writeHead(200);
@@ -50,10 +48,4 @@ var port = 1234;
 server.listen(port);
 var serverUrl = "http://localhost:" + port;
 console.log("open", serverUrl);
-process.argv.slice(2)
-    .forEach(function (f) {
-    var url = serverUrl + "/" + f;
-    console.log('open', url);
-    open(url);
-});
 //# sourceMappingURL=viewer.js.map
